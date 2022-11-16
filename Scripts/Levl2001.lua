8e936df22ea8653a45257281e4863048764817c1
MrKosjaK <misterkosiak@gmail.com>
2022-11-16 17:44:26 +0300

    Update Levl2001.lua

--- Scripts/Levl2001.lua
+++ Scripts/Levl2001.lua
@@ -23,10 +23,10 @@ function _OnLevelInit(level_id)
   AI_SetAways(TRIBE_GREEN, 1, 0, 0, 0, 0);
   AI_SetShamanAway(TRIBE_GREEN, false);
   AI_SetShamanParams(TRIBE_GREEN, 0, 0, false, 0, 12);
-  AI_SetMainDrumTower(TRIBE_RED, false, 0, 0);
-  AI_SetConvertingParams(TRIBE_RED, false, true, 12);
+  AI_SetMainDrumTower(TRIBE_GREEN, false, 0, 0);
+  AI_SetConvertingParams(TRIBE_GREEN, true, true, 12);
 
-  AI_SetBuildingParams(TRIBE_GREEN, false, 0, 0);
+  AI_SetBuildingParams(TRIBE_GREEN, true, 60, 4);
   AI_SetTrainingHuts(TRIBE_GREEN, 0, 0, 0, 0);
   AI_SetTrainingPeople(TRIBE_GREEN, false, 0, 0, 0, 0, 0);
   AI_SetVehicleParams(TRIBE_GREEN, false, 0, 0, 0, 0);
@@ -37,10 +37,75 @@ function _OnLevelInit(level_id)
   AI_SetSpyParams(TRIBE_GREEN, false, 0, 100, 128, 1);
   AI_SetPopulatingParams(TRIBE_GREEN, false, true);
 
-
   -- Cyan stuff
   AI_Initialize(TRIBE_CYAN);
+
+  set_player_can_cast(M_SPELL_BLAST, TRIBE_CYAN);
+  set_player_can_cast(M_SPELL_CONVERT_WILD, TRIBE_CYAN);
+  set_player_can_cast(M_SPELL_LIGHTNING_BOLT, TRIBE_CYAN);
+  set_player_can_cast(M_SPELL_WHIRLWIND, TRIBE_CYAN);
+  set_player_can_cast(M_SPELL_INSECT_PLAGUE, TRIBE_CYAN);
+  set_player_can_cast(M_SPELL_INVISIBILITY, TRIBE_CYAN);
+  set_player_can_cast(M_SPELL_HYPNOTISM, TRIBE_CYAN);
+  set_player_can_cast(M_SPELL_GHOST_ARMY, TRIBE_CYAN);
+  set_player_can_cast(M_SPELL_EARTHQUAKE, TRIBE_CYAN);
+  set_player_can_build(M_BUILDING_TEPEE, TRIBE_CYAN);
+  set_player_can_build(M_BUILDING_DRUM_TOWER, TRIBE_CYAN);
+  set_player_can_build(M_BUILDING_WARRIOR_TRAIN, TRIBE_CYAN);
+  set_player_can_build(M_BUILDING_SUPER_TRAIN, TRIBE_CYAN);
+  set_player_can_build(M_BUILDING_TEMPLE, TRIBE_CYAN);
+
+  AI_SetAways(TRIBE_CYAN, 1, 0, 0, 0, 0);
+  AI_SetShamanAway(TRIBE_CYAN, false);
+  AI_SetShamanParams(TRIBE_CYAN, 0, 0, false, 0, 12);
+  AI_SetMainDrumTower(TRIBE_CYAN, false, 0, 0);
+  AI_SetConvertingParams(TRIBE_CYAN, true, true, 12);
+
+  AI_SetBuildingParams(TRIBE_CYAN, true, 60, 4);
+  AI_SetTrainingHuts(TRIBE_CYAN, 0, 0, 0, 0);
+  AI_SetTrainingPeople(TRIBE_CYAN, false, 0, 0, 0, 0, 0);
+  AI_SetVehicleParams(TRIBE_CYAN, false, 0, 0, 0, 0);
+  AI_SetFetchParams(TRIBE_CYAN, false, false, false, false);
+
+  AI_SetAttackingParams(TRIBE_CYAN, false, 0, 25);
+  AI_SetDefensiveParams(TRIBE_CYAN, true, true, true, true, 3, 1, 1);
+  AI_SetSpyParams(TRIBE_CYAN, false, 0, 100, 128, 1);
+  AI_SetPopulatingParams(TRIBE_CYAN, false, true);
+
+  -- Pink stuff
   AI_Initialize(TRIBE_PINK);
+
+  set_player_can_cast(M_SPELL_BLAST, TRIBE_PINK);
+  set_player_can_cast(M_SPELL_CONVERT_WILD, TRIBE_PINK);
+  set_player_can_cast(M_SPELL_LIGHTNING_BOLT, TRIBE_PINK);
+  set_player_can_cast(M_SPELL_WHIRLWIND, TRIBE_PINK);
+  set_player_can_cast(M_SPELL_INSECT_PLAGUE, TRIBE_PINK);
+  set_player_can_cast(M_SPELL_INVISIBILITY, TRIBE_PINK);
+  set_player_can_cast(M_SPELL_HYPNOTISM, TRIBE_PINK);
+  set_player_can_cast(M_SPELL_GHOST_ARMY, TRIBE_PINK);
+  set_player_can_cast(M_SPELL_EARTHQUAKE, TRIBE_PINK);
+  set_player_can_build(M_BUILDING_TEPEE, TRIBE_PINK);
+  set_player_can_build(M_BUILDING_DRUM_TOWER, TRIBE_PINK);
+  set_player_can_build(M_BUILDING_WARRIOR_TRAIN, TRIBE_PINK);
+  set_player_can_build(M_BUILDING_SUPER_TRAIN, TRIBE_PINK);
+  set_player_can_build(M_BUILDING_TEMPLE, TRIBE_PINK);
+
+  AI_SetAways(TRIBE_PINK, 1, 0, 0, 0, 0);
+  AI_SetShamanAway(TRIBE_PINK, false);
+  AI_SetShamanParams(TRIBE_PINK, 0, 0, false, 0, 12);
+  AI_SetMainDrumTower(TRIBE_PINK, false, 0, 0);
+  AI_SetConvertingParams(TRIBE_PINK, true, true, 12);
+
+  AI_SetBuildingParams(TRIBE_PINK, true, 60, 4);
+  AI_SetTrainingHuts(TRIBE_PINK, 0, 0, 0, 0);
+  AI_SetTrainingPeople(TRIBE_PINK, false, 0, 0, 0, 0, 0);
+  AI_SetVehicleParams(TRIBE_PINK, false, 0, 0, 0, 0);
+  AI_SetFetchParams(TRIBE_PINK, false, false, false, false);
+
+  AI_SetAttackingParams(TRIBE_PINK, false, 0, 25);
+  AI_SetDefensiveParams(TRIBE_PINK, true, true, true, true, 3, 1, 1);
+  AI_SetSpyParams(TRIBE_PINK, false, 0, 100, 128, 1);
+  AI_SetPopulatingParams(TRIBE_PINK, false, true);
 end
 
 function _OnTurn(turn)
