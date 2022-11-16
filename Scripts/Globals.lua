0576af39e3e2053f9af852dfb9a121729451d908
MrKosjaK <misterkosiak@gmail.com>
2022-11-16 11:11:36 +0300

    move imports into globals lua

--- Scripts/Globals.lua
+++ Scripts/Globals.lua
@@ -1,21 +1,26 @@
 -- Imports
+import(Module_Building);
 import(Module_Commands);
 import(Module_DataTypes);
 import(Module_Defines);
 import(Module_Draw);
+import(Module_Features);
 import(Module_Game);
 import(Module_Globals);
 import(Module_Helpers);
-import(Module_Building);
+import(Module_ImGui);
+import(Module_Level);
 import(Module_Map);
-import(Module_Features);
+import(Module_MapWho);
 import(Module_Math);
 import(Module_Objects);
+import(Module_Person);
 import(Module_Players);
 import(Module_PopScript);
+import(Module_Sound);
+import(Module_Spells);
 import(Module_String);
 import(Module_System);
-import(Module_Spells);
 import(Module_Table);
 
 
@@ -46,4 +51,3 @@ G_PLR_PTR = {
 G_SPELL_CONST = spells_type_info();
 G_BLDG_CONST = building_type_info();
 G_ENCY = encyclopedia_info();
-
