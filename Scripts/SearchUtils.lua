372b8018f18b7afecb7a61dbc88bac941e8f8b0d
MrKosjaK <misterkosiak@gmail.com>
2022-11-20 13:45:40 +0300

    Add more search functions

--- Scripts/SearchUtils.lua
+++ Scripts/SearchUtils.lua
@@ -34,3 +34,77 @@ function GetPlayerAreaInfo(player, x, z, rad, tabl)
     return true;
   end);
 end
+
+function GetAllyAreaInfo(player, x, z, rad, tabl)
+  if (tabl == nil or type(tabl) ~= "table") then
+    return;
+  end
+
+  tabl["People"] = {};
+  tabl["Bldgs"] = {};
+
+  for i = 1, 8 do
+    tabl["People"][i] = 0;
+  end
+
+  for j = 1, 19 do
+    tabl["Bldgs"][j] = 0;
+  end
+
+  SearchMapCells(SQUARE, 0, 0, rad, map_xz_to_map_idx(x, z), function(me)
+    me.MapWhoList:processList(function(t)
+      if (are_players_allied(player, t.Owner) == 0) then
+        return true;
+      end
+
+      if (t.Type == T_PERSON) then
+        tabl["People"][t.Model] = tabl["People"][t.Model] + 1;
+        return true;
+      end
+
+      if (t.Type == T_BUILDING) then
+        tabl["Bldgs"][t.Model] = tabl["Bldgs"][t.Model] + 1;
+        return true;
+      end
+      return true;
+    end);
+    return true;
+  end);
+end
+
+function GetEnemyAreaInfo(player, x, z, rad, tabl)
+  if (tabl == nil or type(tabl) ~= "table") then
+    return;
+  end
+
+  tabl["People"] = {};
+  tabl["Bldgs"] = {};
+
+  for i = 1, 8 do
+    tabl["People"][i] = 0;
+  end
+
+  for j = 1, 19 do
+    tabl["Bldgs"][j] = 0;
+  end
+
+  SearchMapCells(SQUARE, 0, 0, rad, map_xz_to_map_idx(x, z), function(me)
+    me.MapWhoList:processList(function(t)
+      if (are_players_enemies(player, t.Owner) == 0) then
+        return true;
+      end
+
+      if (t.Type == T_PERSON) then
+        tabl["People"][t.Model] = tabl["People"][t.Model] + 1;
+        return true;
+      end
+
+      if (t.Type == T_BUILDING) then
+        tabl["Bldgs"][t.Model] = tabl["Bldgs"][t.Model] + 1;
+        return true;
+      end
+      return true;
+    end);
+    return true;
+  end);
+end
