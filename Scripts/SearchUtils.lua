830ca1e8f8e7c92f2ca2c67f9f15a752364835d9
MrKosjaK <misterkosiak@gmail.com>
2022-11-20 11:57:56 +0300

    New helper lua file

---
+++ Scripts/SearchUtils.lua
@@ -0,0 +1,32 @@
+function GetAreaInfo(x, z, rad, tabl)
+  if (tabl == nil or type(tabl) ~= "table") then
+    return;
+  end
+
+  for i = 1, 8 do
+    tabl["People"][i] = 0;
+  end
+
+  for j = 1, 19 do
+    tabl["Bldgs"][j] = 0;
+  end
+
+  SearchMapCells(SQUARE, 0, 0, rad, map_xz_to_map_idx(x, z), function(me)
+    me.MapWhoList:processList(function(t)
+      if (t.Owner == TRIBE_HOSTBOT) then
+        return true;
+      end
+
+      if (t.Type == T_PERSON) then
+        tabl["People"][t.Model] = tabl["People"][t.Model] + 1;
+        return true;
+      end
+
+      if (t.Type == T_BUILDING) then
+        tabl["Bldgs"][t.Model] = tabl["Bldgs"][t.Model] + 1;
+        return true;
+      end
+    end);
+    return true;
+  end);
+end
