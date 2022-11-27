96c839aa9074bcefb2d3485c9491417f6cdc49d4
MrKosjaK <misterkosiak@gmail.com>
2022-11-27 12:01:04 +0300

    guard check

--- Scripts/Libs/LbCommands.lua
+++ Scripts/Libs/LbCommands.lua
@@ -25,14 +25,16 @@ function cmd_mt:get_person_idx(t)
 end
 
 function cmd_mt:attack_person(t, victim_idx)
-  self.CTI.TargetIdx:set(victim_idx);
-  self.CTI.TMIdxs.TargetIdx:set(victim_idx);
+  if (self.Idx < 8) then
+    self.CTI.TargetIdx:set(victim_idx);
+    self.CTI.TMIdxs.TargetIdx:set(victim_idx);
   
-  update_cmd_list_entry(self.Command, CMD_ATTACK_TARGET, self.CTI, 0);
+    update_cmd_list_entry(self.Command, CMD_ATTACK_TARGET, self.CTI, 0);
   
-  t.Flags = t.Flags | TF_RESET_STATE;
-  add_persons_command(t, self.Command, self.Idx);
-  self.Idx = self.Idx + 1;
+    t.Flags = t.Flags | TF_RESET_STATE;
+    add_persons_command(t, self.Command, self.Idx);
+    self.Idx = self.Idx + 1;
+  end
 end
 
 setmetatable(Cmds,
