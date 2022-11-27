68da423f549e61fcac2bde42318fd0087887c66d
MrKosjaK <misterkosiak@gmail.com>
2022-11-27 11:47:42 +0300

    fix command lib not working

--- Scripts/Libs/LbCommands.lua
+++ Scripts/Libs/LbCommands.lua
@@ -7,23 +7,24 @@ Cmds =
 
 local cmd_mt = {};
 
-local function cmd_mt:clear_persons_commands(t)
+function cmd_mt:clear_persons_commands(t)
   self.Idx = 0;
   remove_all_persons_commands(t);
 end
 
-local function cmd_mt:get_person_idx(t)
-  self.Idx = t.u.Pers.CurrCmd + 1;
+function cmd_mt:get_person_idx(t)
+  self.Idx = t.u.Pers.CurrCmd;
 end
 
-local function cmd_mt:attack_person(t, victim_idx)
+function cmd_mt:attack_person(t, victim_idx)
   self.CTI.TargetIdx:set(victim_idx);
   self.CTI.TMIdxs.TargetIdx:set(victim_idx);
   
   update_cmd_list_entry(self.Command, CMD_ATTACK_TARGET, self.CTI, 0);
   
+  t.Flags = t.Flags | TF_RESET_STATE;
   add_persons_command(t, self.Command, self.Idx);
-  self.Idx = Idx + 1;
+  self.Idx = self.Idx + 1;
 end
 
 setmetatable(Cmds,
