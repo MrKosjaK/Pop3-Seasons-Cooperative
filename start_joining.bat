881cb57bf1f4ca5fb74a461d1f094a3c19cafb87
MrKosjaK <misterkosiak@gmail.com>
2025-01-07 01:21:49 +0300

    Add client batch script for joining host

--- start_joining.bat
+++ start_joining.bat
@@ -1,19 +1,25 @@
 @echo off
 
-ECHO Welcome to the Seasons Co-Op Hosting setup script!
+ECHO Welcome to the Seasons Co-Op Connecting setup script!
 ECHO Follow through instructions to configure settings.
 ECHO.
 ECHO.
 
 SET NICK=Player
 SET PORT=7575
+SET IP=127.0.0.1
 SET PLAYER_NUM=1
 
 SET CURR_PATH="%~dp0..\..\"
 PUSHD %CURR_PATH%
 SET CURR_PATH=%CD%
 
-SET /p PORT=Enter port for hosting (default: 7575):
+SET /p IP=Enter IPv4 address of the server (default: 127.0.0.1):
+
+Echo IPv4 address is set to: %IP%
+ECHO.
+
+SET /p PORT=Enter port of the server (default: 7575):
 
 ECHO Port is set to: %PORT%
 ECHO.
@@ -28,9 +34,9 @@ SET /p PLAYER_NUM=Enter the num of the tribe to play as (1 = Blue, 2 = Red, 3 = 
 ECHO Tribe is set to: %PLAYER_NUM%
 ECHO.
 
-START "" %CURR_PATH%\pop3.exe /h %PORT% /n %NICK% /pn %PLAYER_NUM% /mod Pop3-Seasons-Cooperative /multi
+START "" %CURR_PATH%\pop3.exe /j %IP%:%PORT% /n %NICK% /pn %PLAYER_NUM% /mod Pop3-Seasons-Cooperative /multi
 POPD
 
-ECHO Launching game as host...
+ECHO Launching game as client...
 
 PAUSE
