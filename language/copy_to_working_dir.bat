4f846d0cfa33182eeca67ae2a54b3ec2e7ba61fd
MrKosjaK <misterkosiak@gmail.com>
2025-01-07 00:35:04 +0300

    Fix batch script formatting

--- language/copy_to_working_dir.bat
+++ language/copy_to_working_dir.bat
@@ -1,11 +1,11 @@
 @echo off
 
-SET curr_path=%CD%
+SET curr_path=%~dp0
 SET working_dir=%LocalAppData%\Bullfrog\Populous\Language
 
 ECHO Copying language files into working folder!
 
-COPY "%curr_path%\LangStrRep00.ini" "%working_dir%"
+COPY "%curr_path%LangStrRep00.ini" "%working_dir%"
 
 ECHO Finished copying!
 
