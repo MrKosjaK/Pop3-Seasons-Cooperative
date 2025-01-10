02d28492539f28901a1a26b8c92c5a3eba072400
MrKosjaK <misterkosiak@gmail.com>
2025-01-11 00:27:51 +0300

    Single batch script for online prep

--- copy_for_online.bat
+++ copy_for_online.bat
@@ -24,6 +24,29 @@ COPY "%LANG_PATH%\LangStrRep00.ini" "%WORK_DIR%\language"
 
 :: Levels
 ECHO.
+ECHO Converting singleplayer levels into multiplayer levels...
+
+COPY "%LVL_PATH%\levl2001.dat" "%LVL_PATH%\levl2080.dat"
+COPY "%LVL_PATH%\levl2002.dat" "%LVL_PATH%\levl2082.dat"
+COPY "%LVL_PATH%\levl2003.dat" "%LVL_PATH%\levl2083.dat"
+COPY "%LVL_PATH%\levl2004.dat" "%LVL_PATH%\levl2084.dat"
+
+COPY "%LVL_PATH%\levl2005.dat" "%LVL_PATH%\levl2094.dat"
+COPY "%LVL_PATH%\levl2006.dat" "%LVL_PATH%\levl2100.dat"
+COPY "%LVL_PATH%\levl2007.dat" "%LVL_PATH%\levl2109.dat"
+COPY "%LVL_PATH%\levl2008.dat" "%LVL_PATH%\levl2110.dat"
+
+COPY "%LVL_PATH%\levl2009.dat" "%LVL_PATH%\levl2111.dat"
+COPY "%LVL_PATH%\levl2010.dat" "%LVL_PATH%\levl2112.dat"
+COPY "%LVL_PATH%\levl2011.dat" "%LVL_PATH%\levl2120.dat"
+COPY "%LVL_PATH%\levl2012.dat" "%LVL_PATH%\levl2127.dat"
+
+COPY "%LVL_PATH%\levl2013.dat" "%LVL_PATH%\levl2128.dat"
+COPY "%LVL_PATH%\levl2014.dat" "%LVL_PATH%\levl2131.dat"
+COPY "%LVL_PATH%\levl2015.dat" "%LVL_PATH%\levl2133.dat"
+COPY "%LVL_PATH%\levl2016.dat" "%LVL_PATH%\levl2134.dat"
+
+ECHO.
 ECHO Copying level files...
 
 COPY "%LVL_PATH%\levl2080.dat" "%WORK_DIR%\levels"
