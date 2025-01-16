7ec1ef20a7c77431ea8d608d8e068f6d8964fa99
MrKosjaK <misterkosiak@gmail.com>
2025-01-16 14:33:55 +0300

    Simple OnPacket test

--- scripts/common.lua
+++ scripts/common.lua
@@ -43,9 +43,26 @@ function OnKeyDown(key)
 end
 
 
+PACKET_GIVE_SPELL_ADD = 1;
+PACKET_GIVE_SPELL_SUB = 2;
+
 -- triggered on releasing pressed key
 function OnKeyUp(key)
   if (ScrOnKeyUp ~= nil) then ScrOnKeyUp(key); end
+  
+  -- network based/online
+  if (am_i_in_network_game() ~= 0) then
+    -- just a test
+    if (key == LB_KEY_1) then
+      -- addition
+      if (_IsKeyDown(LB_KEY_LSHIFT) or _IsKeyDown(LB_KEY_RSHIFT)) then
+        Send(PACKET_GIVE_SPELL_ADD, tostring(key));
+      -- subtruction
+      elseif (_IsKeyDown(LB_KEY_LCONTROL) or _IsKeyDown(LB_KEY_RCONTROL)) then
+        Send(PACKET_GIVE_SPELL_SUB, tostring(key));
+      end
+    end
+  end
 end
 
 
@@ -53,5 +70,21 @@ end
 function OnPacket(player_num, packet_type, data)
   if (am_i_in_network_game() ~= 0) then
     if (ScrOnPacket ~= nil) then ScrOnPacket(player_num, packet_type, data); end
+    
+    if (packet_type == PACKET_GIVE_SPELL_ADD) then
+      local key = tonumber(data);
+      
+      if (key == LB_KEY_1) then
+        increment_number_of_one_shots(player_num, M_SPELL_BLAST);
+      end
+    end
+    
+    if (packet_type == PACKET_GIVE_SPELL_SUB) then
+      local key = tonumber(data);
+      
+      if (key == LB_KEY_1) then
+        reduce_number_of_one_shots(player_num, M_SPELL_BLAST);
+      end
+    end
   end
 end
