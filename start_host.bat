8033d7df1e8639913d4bfee1c7ffc266df972549
MrKosjaK <misterkosiak@gmail.com>
2025-02-01 21:01:14 +0300

    Fix weird desync using 127.0.0.1 ip as host

--- start_host.bat
+++ start_host.bat
@@ -7,17 +7,11 @@ ECHO.
 
 SET NICK=Host
 SET PORT=7575
-SET IP=127.0.0.1
 SET PLAYER_NUM=1
 
 SET CURR_PATH="%~dp0..\..\"
 PUSHD %CURR_PATH%
 SET CURR_PATH=%CD%
-
-SET /p IP=Enter IPv4 address of the server (default: 127.0.0.1):
-
-Echo IPv4 address is set to: %IP%
-ECHO.
 
 SET /p PORT=Enter port for hosting (default: 7575):
 
@@ -34,7 +28,7 @@ SET /p PLAYER_NUM=Enter the num of the tribe to play as (1 = Blue, 2 = Red, 3 = 
 ECHO Tribe is set to: %PLAYER_NUM%
 ECHO.
 
-START "" "%CURR_PATH%\pop3.exe" /h %IP%:%PORT% /n %NICK% /pn %PLAYER_NUM% /mod Pop3-Seasons-Cooperative /multi
+START "" "%CURR_PATH%\pop3.exe" /h %PORT% /n %NICK% /pn %PLAYER_NUM% /mod Pop3-Seasons-Cooperative /multi
 POPD
 
 ECHO Launching game as host...
