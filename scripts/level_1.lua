b957b80ee97e29b85b5e2083f76af46eb9de3758
MrKosjaK <misterkosiak@gmail.com>
2025-02-08 21:05:58 +0300

    Snow in lvl1

--- scripts/level_1.lua
+++ scripts/level_1.lua
@@ -1,5 +1,6 @@
 include("common.lua");
 include("pop_helper.lua");
+include("weather.lua");
 
 set_level_human_count(2);
 set_level_computer_count(1);
@@ -9,7 +10,7 @@ add_human_player_start_info(1, {M_SPELL_BLAST, M_SPELL_CONVERT_WILD, M_SPELL_HYP
 add_ai_player_start_info(2, 3, {M_SPELL_BLAST, M_SPELL_CONVERT_WILD}, {M_BUILDING_TEPEE, M_BUILDING_DRUM_TOWER, M_BUILDING_WARRIOR_TRAIN, M_BUILDING_SUPER_WARRIOR_TRAIN});
 
 function OnInit()
-  
+  start_weather(WEATHER_SNOW, 750, 75, 35, 60*10) -- type, amount, ~per_second_spawn, speed, duration_seconds
 end
 
 function ScrOnLevelInit(level_id)
@@ -17,6 +18,7 @@ function ScrOnLevelInit(level_id)
 end
 
 function ScrOnTurn(curr_turn)
+  process_weather(curr_turn);
   calculate_population_scores();
 end
 
