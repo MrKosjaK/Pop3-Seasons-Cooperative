eba428c1cc504f2112948b157bb6434f4fc3053a
MrKosjaK <misterkosiak@gmail.com>
2025-02-09 20:06:12 +0300

    Remove test AI code

--- scripts/game_lobby.lua
+++ scripts/game_lobby.lua
@@ -251,41 +251,6 @@ function spawn_players_initial_stuff();
     G_PLR[p_num].ReincarnSiteCoord.Zpos = ai_data._start_pos.Zpos;
     mark_reincarnation_site_mes(G_PLR[p_num].ReincarnSiteCoord, p_num, MARK);
     set_players_shaman_initial_command(G_PLR[p_num]);
-    
-    -- TEST AI STUFF
-
-    STATE_SET(p_num, TRUE, CP_AT_TYPE_CONSTRUCT_BUILDING);
-    STATE_SET(p_num, TRUE, CP_AT_TYPE_AUTO_ATTACK);
-    STATE_SET(p_num, TRUE, CP_AT_TYPE_TRAIN_PEOPLE);
-    SET_ATTACK_VARIABLE(p_num, 0);
-    WRITE_CP_ATTRIB(p_num, ATTR_MAX_BUILDINGS_ON_GO, 3);
-    WRITE_CP_ATTRIB(p_num, ATTR_HOUSE_PERCENTAGE, 30);
-    WRITE_CP_ATTRIB(p_num, ATTR_PREF_WARRIOR_TRAINS, 1);
-    WRITE_CP_ATTRIB(p_num, ATTR_PREF_WARRIOR_PEOPLE, 0);
-    WRITE_CP_ATTRIB(p_num, ATTR_MAX_TRAIN_AT_ONCE, 4);
-    WRITE_CP_ATTRIB(p_num, ATTR_MAX_ATTACKS, 3);
-    WRITE_CP_ATTRIB(p_num, ATTR_ATTACK_PERCENTAGE, 100);
-    WRITE_CP_ATTRIB(p_num, ATTR_MAX_DEFENSIVE_ACTIONS, 0);
-    WRITE_CP_ATTRIB(p_num, ATTR_RETREAT_VALUE, 0);
-    WRITE_CP_ATTRIB(p_num, ATTR_BASE_UNDER_ATTACK_RETREAT, 0);
-
-    WRITE_CP_ATTRIB(p_num, ATTR_EXPANSION, 28);
-    WRITE_CP_ATTRIB(p_num, ATTR_SHAMEN_BLAST, 16);
-    WRITE_CP_ATTRIB(p_num, ATTR_PREF_SUPER_WARRIOR_TRAINS, 1);
-    STATE_SET(p_num, TRUE, CP_AT_TYPE_MED_MAN_GET_WILD_PEEPS);
-    STATE_SET(p_num, TRUE, CP_AT_TYPE_FETCH_WOOD);
-    STATE_SET(p_num, TRUE, CP_AT_TYPE_BRING_NEW_PEOPLE_BACK);
-    STATE_SET(p_num, TRUE, CP_AT_TYPE_POPULATE_DRUM_TOWER);
-
-    SET_BUCKET_USAGE(p_num, TRUE);
-    SET_BUCKET_COUNT_FOR_SPELL(p_num, M_SPELL_CONVERT_WILD, 1);
-    SET_BUCKET_COUNT_FOR_SPELL(p_num, M_SPELL_BLAST, 1);
-    SET_BUCKET_COUNT_FOR_SPELL(p_num, M_SPELL_INSECT_PLAGUE, 8);
-    
-    local map_xz = coord3d_to_map_xz(ai_data._start_pos);
-    SET_DRUM_TOWER_POS(p_num, map_xz.XZ.X, map_xz.XZ.Z);
-    SHAMAN_DEFEND(p_num, map_xz.XZ.X, map_xz.XZ.Z, TRUE);
-    SET_DEFENCE_RADIUS(p_num, 5);
   end
 end
 
