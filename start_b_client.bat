7590a1e90061f840261bd4be0956c9c891020ae0
MrKosjaK <misterkosiak@gmail.com>
2025-07-05 22:27:30 +0300

    Add pop3b version of bat files

---
+++ start_b_client.bat
@@ -0,0 +1,40 @@
+@echo off
+
+ECHO Welcome to the Seasons Co-Op Connecting setup script!
+ECHO Follow through instructions to configure settings.
+ECHO.
+ECHO.
+
+SET NICK=Player
+SET PORT=7575
+SET IP=127.0.0.1
+SET PLAYER_NUM=1
+
+SET CURR_PATH="%~dp0..\..\"
+PUSHD %CURR_PATH%
+SET CURR_PATH=%CD%
+
+SET /p IP=Enter IPv4 address of the server (default: 127.0.0.1):
+
+Echo IPv4 address is set to: %IP%
+ECHO.
+
+SET /p PORT=Enter port of the server (default: 7575):
+
+ECHO Port is set to: %PORT%
+ECHO.
+
+SET /p NICK=Enter your player nickname (default: Player):
+
+ECHO Nickname is set to: %NICK%
+ECHO.
+
+SET /p PLAYER_NUM=Enter the num of the tribe to play as (1 = Blue, 2 = Red, 3 = Yellow etc...):
+
+ECHO Tribe is set to: %PLAYER_NUM%
+ECHO.
+
+START "" "%CURR_PATH%\pop3b.exe" /j 127.0.0.1:%PORT% /n %NICK% /pn %PLAYER_NUM% /mod Pop3-Seasons-Cooperative /multi 
+POPD
+
+ECHO Launching game as client...
---
+++ start_b_host.bat
@@ -0,0 +1,34 @@
+@echo off
+
+ECHO Welcome to the Seasons Co-Op Hosting setup script!
+ECHO Follow through instructions to configure settings.
+ECHO.
+ECHO.
+
+SET NICK=Host
+SET PORT=7575
+SET PLAYER_NUM=1
+
+SET CURR_PATH="%~dp0..\..\"
+PUSHD %CURR_PATH%
+SET CURR_PATH=%CD%
+
+SET /p PORT=Enter port for hosting (default: 7575):
+
+ECHO Port is set to: %PORT%
+ECHO.
+
+SET /p NICK=Enter your player nickname (default: Host):
+
+ECHO Nickname is set to: %NICK%
+ECHO.
+
+SET /p PLAYER_NUM=Enter the num of the tribe to play as (1 = Blue, 2 = Red, 3 = Yellow etc...):
+
+ECHO Tribe is set to: %PLAYER_NUM%
+ECHO.
+
+START "" "%CURR_PATH%\pop3b.exe" /h %PORT% /n %NICK% /pn %PLAYER_NUM% /mod Pop3-Seasons-Cooperative /multi
+POPD
+
+ECHO Launching game as host...
